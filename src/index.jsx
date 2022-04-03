1c2cca7970b885c9bca68b6097c39a7bf4892729
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-03 03:15:27 +0300

    build: adding createRoot (React 18)

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+// import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+// import { store } from './app/store';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
