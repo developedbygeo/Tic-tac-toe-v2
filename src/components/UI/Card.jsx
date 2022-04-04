44909201276215478aa853bdb26a96d01d7ded5f
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-05 00:11:29 +0300

    feat: adding Card and ScoreCard

---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+import { ScoreCardType } from '../../theme';
+
+export const Card = ({ children, className }) => (
+  <div className={`custom-card  ${className || ''}`}>{children}</div>
+);
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string.isRequired
+};
+
+export const ScoreCard = ({ title, score, type, className }) => (
+  <div
+    className={`text-center text-dark-navy rounded-lg w-11/12 h-15 px-2 py-1 ${
+      ScoreCardType[type]
+    }  ${className || ''}`}
+  >
+    <span className="text-base tracking-wide">{title}</span>
+    <p className="font-bold text-xl">{score}</p>
+  </div>
+);
+
+ScoreCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  className: PropTypes.string,
+  type: PropTypes.oneOf(Object.keys(ScoreCardType)).isRequired
+};
+ScoreCard.defaultProps = {
+  className: ''
+};
