cd38dfe48621cf6f95ebd88ce2e3285486be388a
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-05 00:04:46 +0300

    feat: adding the protected game route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Menu from './pages/Menu';
+import Game from './pages/Game';
 
-const App = () => (
-  <main className="bg-dark-navy text-silver h-full w-full flex  items-center justify-center">
-    <Routes>
-      <Route path="/" element={<Navigate to="/menu" />} />
-      <Route path="/menu" element={<Menu />} />
-    </Routes>
-  </main>
-);
+const App = () => {
+  const { mode } = useSelector((state) => state.game);
+
+  return (
+    <main className="bg-dark-navy text-silver h-full w-full flex  items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Navigate to="menu" />} />
+        <Route path="menu" element={<Menu />} />
+        <Route path="game" element={mode ? <Game /> : <Navigate to="/menu" />} />
+      </Routes>
+    </main>
+  );
+};
 
 export default App;
