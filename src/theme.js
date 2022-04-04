f993c1552423375b09d54cd46897fd0d68982591
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-05 00:05:54 +0300

    style: adding pre-config styling props

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const ButtonType = {
+  primary:
+    'btn bg-light-yellow shadow-primaryButtonOne hover:bg-light-yellow-hover active:shadow-primaryButtonOneActive',
+  secondary:
+    'btn bg-light-blue shadow-primaryButtonTwo hover:bg-light-blue-hover active:shadow-primaryButtonTwoActive',
+  basic:
+    'btn bg-custom-silver shadow-secondaryButton hover:bg-custom-silver-hover active:shadow-secondaryButtonActive'
+};
+
+export const ScoreCardType = {
+  X: 'bg-light-blue',
+  Ties: 'bg-custom-silver',
+  O: 'bg-light-yellow'
+};
+
+export const ButtonSize = {
+  lg: 'py-3 px-6 text-lg',
+  xl: 'px-8,  py-32'
+};
