ed409459e1088212982ef1117be0f292260e2e2a
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-05 00:10:52 +0300

    style: adding styling props to the custom button

--- src/components/UI/Buttons.jsx
+++ src/components/UI/Buttons.jsx
@@ -1,16 +1,31 @@
 import PropTypes from 'prop-types';
 
-const CustomButton = ({ children, onClick, className, title }) => (
-  <button title={title} onClick={onClick} type="button" className={`btn ${className}`}>
+import { ButtonType, ButtonSize } from '../../theme';
+
+const CustomButton = ({ children, onClick, className, type, size, title }) => (
+  <button
+    title={title}
+    onClick={onClick}
+    type="button"
+    className={`btn ${ButtonType[type]} ${ButtonSize[size]} ${className}`}
+  >
     {children}
   </button>
 );
 
 CustomButton.propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  type: PropTypes.oneOf(Object.keys(ButtonType)),
+  size: PropTypes.oneOf(Object.keys(ButtonSize)),
   onClick: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired
+};
+
+CustomButton.defaultProps = {
+  type: null,
+  size: null,
+  className: ''
 };
 
 export default CustomButton;
