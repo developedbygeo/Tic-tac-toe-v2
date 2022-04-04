87ef5e402fdccf14bfb08df092fbf7e325568a72
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-05 00:05:25 +0300

    feat: adding the provider and store

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
-// import { store } from './app/store';
+import store from './app/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>
 );
