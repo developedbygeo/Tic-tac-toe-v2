d9c73f37ac34071fcef9eda539aa89964b54029f
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-08 00:38:05 +0300

    feat: fixing turn and rendering modal

--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -1,24 +1,62 @@
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import { Card } from './UI/Card';
 import CustomButton from './UI/Buttons';
 import { ReactComponent as Logo } from '../assets/logo.svg';
 import { ReactComponent as Restart } from '../assets/icon-restart.svg';
 
-// TODO add logo link to menu page
+import Modal from './UI/Modal';
+import GameOverDialogue from './GameOverDialogue';
+import GameRestartDialogue from './GameRestartDialogue';
 
 const GameHeader = () => {
-  const { currentTurn } = useSelector((state) => state.game);
+  const [showModal, setShowModal] = useState(true);
+  const [restartModal, setRestartModal] = useState(false);
+  const { currentTurn, finalState, winner } = useSelector((state) => state.game);
+  const finalStateText = finalState && winner ? `${winner} ${finalState}` : `${finalState}`;
+  const turnText = `${currentTurn}`;
+
+  const handleQuit = () => setShowModal((prevState) => !prevState);
+
+  const handleRestart = () => {
+    setRestartModal((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    if (finalState) {
+      setShowModal(true);
+    }
+  }, [finalState]);
 
   return (
-    <div className="w-full h-fit flex flex-row justify-between items-center">
-      <Logo />
-      <Card className="px-4 py-2 text-custom-silver font-bold flex flex-row gap-2 items-center justify-center">
-        <span>{currentTurn}</span> <span>TURN</span>
-      </Card>
-      <CustomButton type="basic" className="p-3">
-        <Restart />
-      </CustomButton>
-    </div>
+    <>
+      <div className="w-full h-fit flex flex-row justify-between items-center">
+        <Link to="/">
+          <Logo />
+        </Link>
+        <Card className="px-4 py-2 text-custom-silver font-bold flex flex-row gap-1 items-center justify-center text-center">
+          <span className="font-bold w-full text-center">
+            {finalState ? finalStateText : turnText}
+          </span>
+          <span>{!finalState && 'TURN'}</span>
+        </Card>
+        <CustomButton onClick={handleRestart} title="Restart game" type="basic" className="p-3">
+          <Restart />
+        </CustomButton>
+      </div>
+      {finalState && showModal && (
+        <Modal>
+          <GameOverDialogue onDisable={handleQuit} />
+        </Modal>
+      )}
+      {restartModal && (
+        <Modal onDisable={handleRestart}>
+          <GameRestartDialogue onDisable={handleRestart} />
+        </Modal>
+      )}
+    </>
   );
 };
 
