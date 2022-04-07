72aae49c17f10e11065a2daac41b51d88cfa01de
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-08 00:40:40 +0300

    refactor: uniform naming convention and adding 2 new lookups

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,22 +3,31 @@ import { v4 as uuidv4 } from 'uuid';
 import outlineX from '../assets/icon-x-outline.svg';
 import outlineO from '../assets/icon-o-outline.svg';
 
+import { ReactComponent as MarkX } from '../assets/icon-x.svg';
+import { ReactComponent as MarkO } from '../assets/icon-o.svg';
+
 export const hoverLookup = {
   X: outlineX,
   O: outlineO
 };
 
-export const MARKS = {
+export const svgLookup = {
+  X: <MarkX />,
+  O: <MarkO />,
+  '': ''
+};
+
+export const marksLookup = {
   X: 'X',
   O: 'O'
 };
 
-export const SELECTION_MARKS = [
+export const selectionMarksLookup = [
   { mark: 'X', id: uuidv4() },
   { mark: 'O', id: uuidv4() }
 ];
 
-export const WINNING_COMBOS = [
+export const winningCombosLookup = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -28,3 +37,15 @@ export const WINNING_COMBOS = [
   [1, 4, 7],
   [2, 5, 8]
 ];
+
+export const cpuDialogues = {
+  win: 'YOU WON',
+  loss: 'OH NO, YOU LOST...'
+};
+export const p2Dialogues = (player) => `${player} WON`;
+
+export const winnerColorLookup = {
+  X: '#31C3BD',
+  O: '#F2B137',
+  null: '#A8BFC9'
+};
