4fb12bd9b3215fa258fbc17657b7808db74d95e9
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-09 03:54:01 +0300

    refactor/style: adding breakpoints and introducing helpers

--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -7,6 +7,8 @@ import CustomButton from './UI/Buttons';
 import { ReactComponent as Logo } from '../assets/logo.svg';
 import { ReactComponent as Restart } from '../assets/icon-restart.svg';
 
+import { gameStatusCommentary, turnOrWinner } from '../utils/utilFns';
+
 import Modal from './UI/Modal';
 import GameOverDialogue from './GameOverDialogue';
 import GameRestartDialogue from './GameRestartDialogue';
@@ -15,8 +17,9 @@ const GameHeader = () => {
   const [showModal, setShowModal] = useState(true);
   const [restartModal, setRestartModal] = useState(false);
   const { currentTurn, finalState, winner } = useSelector((state) => state.game);
-  const finalStateText = finalState && winner ? `${winner} ${finalState}` : `${finalState}`;
-  const turnText = `${currentTurn}`;
+
+  const turnText = turnOrWinner(finalState, winner, currentTurn);
+  const currentStatusText = gameStatusCommentary(finalState, winner);
 
   const handleQuitModal = () => setShowModal((prevState) => !prevState);
 
@@ -34,17 +37,19 @@ const GameHeader = () => {
         <Link to="/">
           <Logo />
         </Link>
-        <Card className="px-4 py-2 text-custom-silver font-bold flex flex-row gap-1 items-center justify-center text-center">
-          <span className="font-bold w-full text-center">
-            {finalState ? finalStateText : turnText}
+        <Card className="px-4 py-2 text-custom-silver font-bold flex flex-row gap-1 items-center justify-center text-center md:px-10 md:py-3">
+          {turnText && (
+            <span className="font-bold w-full text-center text-sm md:text-xl">{turnText}</span>
+          )}
+          <span className="font-bold w-full text-center text-sm md:text-xl">
+            {currentStatusText}
           </span>
-          <span>{!finalState && 'TURN'}</span>
         </Card>
         <CustomButton
           onClick={handleRestartModal}
           title="Restart game"
           type="basic"
-          className="p-3"
+          className="px-6 py-3"
         >
           <Restart />
         </CustomButton>
