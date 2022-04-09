c15f27eb209b779ef4e602264e60667488e26550
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-10 02:20:50 +0300

    style: assigning landscape classes

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -12,10 +12,10 @@ const Game = () => {
   const { mark: mark2, id: id2 } = players.find((player) => player.mark === 'O');
 
   return (
-    <section className="h-5/6 mt-5 mb-auto w-11/12 flex flex-col justify-between md:justify-center md:w-9/12 md:m-auto md:gap-8 xl:h-5/6 xl:w-5/12 2xl:w-4/12">
+    <section className="h-5/6 mt-5 mb-auto w-11/12 flex flex-col justify-between md:justify-center md:w-9/12 md:m-auto md:gap-8 lg:h-5/6 lg:w-5/12 2xl:w-4/12 landmobileSS:landscape-game">
       <GameHeader />
       <GameBoard />
-      <article className="w-full grid grid-cols-3 gap-2 place-items-center">
+      <article className="w-full grid grid-cols-3 gap-2 place-items-center landmobileSS:landscape-score-cont">
         <ScoreCard title={`${mark1} (${id1})`} score={winsX} type="X" />
         <ScoreCard title="Ties" score={ties} type="Ties" />
         <ScoreCard title={`${mark2} (${id2})`} score={winsO} type="O" />
