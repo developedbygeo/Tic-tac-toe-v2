474a46a3d0f0ba7c8cf6c0ad531e6742c69fb502
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-10 02:21:31 +0300

    style: assigning landscape classes

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -40,18 +40,20 @@ const Menu = () => {
   };
 
   return (
-    <section className="game-cont flex flex-col gap-5 justify-evenly items-center h-4/6 w-5/6 md:w-7/12 md:justify-center md:gap-8 xl:h-5/6 xl:w-5/12 2xl:w-4/12">
+    <section className="flex flex-col gap-5 justify-evenly items-center h-4/6 w-5/6 md:w-7/12 md:justify-center md:gap-8 xl:h-5/6 xl:w-5/12 2xl:w-4/12 landmobileSS:landscape-menu">
       <div className="py-2">
         <Logo />
       </div>
-      <Card className="h-3/6 w-full flex flex-col justify-between items-center py-4 text-custom-silver md:h-1/4 xl:h-2/6">
-        <h1 className="font-bold uppercase tracking-wider md:text-xl">pick player 1&apos;s mark</h1>
+      <Card className="h-3/6 w-full flex flex-col justify-between items-center py-4 text-custom-silver md:h-1/4 lg:h-1/3 xl:h-2/6 landmobileSS:landscape-selection-menu">
+        <h1 className="font-bold text-s uppercase tracking-wider landmobileSS:text-base">
+          pick player 1&apos;s mark
+        </h1>
         <MenuSelect onMarkSelect={markSelectionHandler} selected={selectedMark} />
-        <h2 className="font-normal uppercase tracking-wide text-sm opacity-50 md:text-lg">
+        <h2 className="font-normal uppercase tracking-wide text-base opacity-50">
           Remember: x goes first
         </h2>
       </Card>
-      <div className="flex flex-col gap-5 w-full">
+      <div className="flex flex-col gap-5 w-full landmobileSS:landscape-selection-buttons">
         <CustomButton
           title="Play against the computer"
           type="primary"
