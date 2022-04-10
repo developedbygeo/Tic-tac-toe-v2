dad3f904def50823061d7c0a3cba56fca8d6c115
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-11 01:22:23 +0300

    feat: adding 404 redirection

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
     <main className="bg-dark-navy text-silver h-full w-full flex  items-center justify-center">
       <Routes>
         <Route path="/" element={<Navigate to="menu" />} />
+        <Route path="*" element={<Navigate to="menu" />} />
         <Route path="menu" element={<Menu />} />
         <Route path="game" element={mode ? <Game /> : <Navigate to="/menu" />} />
       </Routes>
