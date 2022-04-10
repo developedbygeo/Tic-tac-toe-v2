ebfba5bd313aaebe22a2563eac49baf941dad812
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-10 19:12:03 +0300

    style: adding the new landscape tablet class

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -40,11 +40,11 @@ const Menu = () => {
   };
 
   return (
-    <section className="flex flex-col gap-5 justify-evenly items-center h-4/6 w-5/6 md:w-7/12 md:justify-center md:gap-8 xl:h-5/6 xl:w-5/12 2xl:w-4/12 landmobileSS:landscape-menu">
+    <section className="flex flex-col gap-5 justify-evenly items-center h-4/6 w-5/6 md:w-7/12 md:justify-center md:gap-8 xl:h-5/6 xl:w-5/12 2xl:w-4/12 landmobileSS:landscape-menu landscapeLargeTablet:h-[100%]">
       <div className="py-2">
         <Logo />
       </div>
-      <Card className="h-3/6 w-full flex flex-col justify-between items-center py-4 text-custom-silver md:h-1/4 lg:h-1/3 xl:h-2/6 landmobileSS:landscape-selection-menu">
+      <Card className="h-3/6 w-full flex flex-col justify-between items-center py-4 text-custom-silver md:h-1/4 lg:h-1/3 xl:h-2/6 landmobileSS:landscape-selection-menu landscapeLargeTablet:landscape-tablet-menu">
         <h1 className="font-bold text-s uppercase tracking-wider landmobileSS:text-base">
           pick player 1&apos;s mark
         </h1>
