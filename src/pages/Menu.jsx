403df3e3d32ed30bb679774af9944f760af15127
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-11 01:23:40 +0300

    style: adjusting height

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -40,7 +40,7 @@ const Menu = () => {
   };
 
   return (
-    <section className="flex flex-col gap-5 justify-evenly items-center h-4/6 w-5/6 md:w-7/12 md:justify-center md:gap-8 xl:h-5/6 xl:w-5/12 2xl:w-4/12 landmobileSS:landscape-menu landscapeLargeTablet:h-[100%]">
+    <section className="flex flex-col gap-5 justify-evenly items-center h-4/6 w-5/6 md:w-7/12 md:justify-center md:gap-8 lg:max-w-md xl:h-5/6 xl:w-5/12 2xl:w-4/12 landmobileSS:landscape-menu landscapeLargeTablet:h-[100%]">
       <div className="py-2">
         <Logo />
       </div>
